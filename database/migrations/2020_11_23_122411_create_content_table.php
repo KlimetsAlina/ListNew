8328eb2452d47c78272ca0af3a429f8727987636
Alina Klimets <alinkaklim@yandex.ru>
2020-11-26 12:45:32 +0300

    Миграции: создание таблиц для контента

---
+++ database/migrations/2020_11_23_122411_create_content_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('author');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content');
+    }
+}
