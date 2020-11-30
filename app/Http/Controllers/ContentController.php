270b4e0a2e3dbdb23eadc5fd6926a3f289feb6a5
Alina Klimets <alinkaklim@yandex.ru>
2020-11-30 19:01:41 +0300

    Правки роута

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -6,38 +6,19 @@ use App\Content;
 
 class ContentController extends Controller
 {
-    public function getFilms()
-    {
-        return $this->getUserContentByType('film');
-    }
-
-    public function getSerials()
-    {
-        return $this->getUserContentByType('serial');
-    }
-
-    public function getBooks()
-    {
-        return $this->getUserContentByType('book');
-    }
-
-    public function getMusic()
-    {
-        return $this->getUserContentByType('music');
-    }
-
-    public function getOther()
-    {
-        return $this->getUserContentByType('other');
-    }
-
-    public function getUserContentByType(string $type)
+    public function getContent($type)
     {
         $content = Content::where('type', $type)->get();
 
-        return view('list', [
-            'contentType' => $type,
-            'content'     => $content,
-        ]);
+        if ($content) {
+            return view('list', [
+                'contentType' => $type,
+                'content'     => $content,
+            ]);
+        }
+
+        /* Когда контента нет return отдельн view
+        или нет  */
+        return view('home');
     }
 }
