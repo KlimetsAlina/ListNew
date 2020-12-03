f28aec4e6126ba25ff6e4e5bf152cadc562e06aa
Alina Klimets <alinkaklim@yandex.ru>
2020-12-03 18:27:14 +0300

    Фронт: изменения верстки, добавление пакета

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Menu;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', [
+            'menu' => Menu::all(),
+        ]);
     }
 }
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,73 +1,71 @@
-@extends('page')
+@extends('layouts.app')
 
 @section('content')
-    <div id="allcontent">
-        @if(!empty($content))
-            <div class="list">
-                <h2>
-                    @switch($contentType)
-                        @case('music')
-                        Прослушанное
-                        @break
 
-                        @case('book')
-                        Прочитанное
-                        @break
+    <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+            <h2>
+                @switch($contentType)
+                    @case('music')
+                    Прослушанное
+                    @break
 
-                        @default
-                        Просмотренное
-                    @endswitch
-                </h2>
-                <ul>
-                    @foreach($content as $item)
-                        @if($item->pivot->watched)
-                            <li>
-                                {{ $item->name }}
-                                @if($item->author)- {{$item->author}}  @endif
-                                <a class="del_a" onclick="select_content(this);"></a>
-                            </li>
-                        @endif
-                    @endforeach
-                    <?php
-                    //    Старинная реализация
-                    // $arr_length = count($array_content1);
-                    // for ($i = 0; $i < $arr_length; $i++) {
-                    //     echo '<li>' . $array_content1[$i] . '<a class="del_a" onclick="select_content(this);"></a></li>';
-                    // }
-                    // ?>
-                    <li id="show1" onclick="setwatch(1)"> +</li>
-                </ul>
-            </div>
-        @endif
+                    @case('book')
+                    Прочитанное
+                    @break
 
-        @if(!empty($content))
-            <div class="list">
-                <h2> Хочу
-                    @switch($contentType)
-                        @case('music')
-                     послушать
-                        @break
-                        @case('book')
-                     прочесть
-                        @break
-                        @default
-                     посмотреть
-                    @endswitch
-                </h2>
-                <ul>
-                    @foreach($content as $item)
-                        @if(!$item->pivot->watched)
-                            <li>
-                                {{ $item->name }}
-                                @if($item->author)- {{$item->author}}  @endif
-                                <a class="del_a" onclick="select_content(this);"></a>
-                            </li>
-                        @endif
-                    @endforeach
-                    <li id="show0" onclick="setwatch(0)"> +</li>
-                </ul>
-            </div>
-        @endif
+                    @default
+                    Просмотренное
+                @endswitch
+            </h2>
+            <ul class="list-group list-group-flush">
+
+            @foreach($content as $item)
+                    @if($item->pivot->watched)
+                        <li class="list-group-item">
+
+                            {{ $item->name }}
+                            @if($item->author)- {{$item->author}}  @endif
+                            <a class="del_a" onclick="select_content(this);"> -</a>
+                    @endif
+                @endforeach
+                <?php
+                //    Старинная реализация
+                // $arr_length = count($array_content1);
+                // for ($i = 0; $i < $arr_length; $i++) {
+                //     echo '<li>' . $array_content1[$i] . '<a class="del_a" onclick="select_content(this);"></a></li>';
+                // }
+                // ?>
+                <li class="list-group-item" id="show0" onclick="setwatch(0)"> +</li>
+            </ul>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+            <h2> Хочу
+                @switch($contentType)
+                    @case('music')
+                 послушать
+                    @break
+                    @case('book')
+                 прочесть
+                    @break
+                    @default
+                 посмотреть
+                @endswitch
+            </h2>
+
+            <ul class="list-group list-group-flush">
+                @foreach($content as $item)
+                    @if(!$item->pivot->watched)
+                        <li class="list-group-item">
+                            {{ $item->name }}
+                            @if($item->author)- {{$item->author}}  @endif
+                            <a class="del_a" onclick="select_content(this);"></a>
+                        </li>
+                    @endif
+                @endforeach
+                <li class="list-group-item" id="show0" onclick="setwatch(0)"> +</li>
+            </ul>
+        </div>
     </div>
 
     <dialog class="dialog" id="add">
