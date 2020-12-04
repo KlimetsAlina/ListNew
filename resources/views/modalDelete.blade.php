ee2ef10a8303edec7b2dbefbe6d24ae83a25672f
Alina Klimets <alinkaklim@yandex.ru>
2020-12-04 18:23:02 +0300

    Фронт: добавление модалки для удаления

---
+++ resources/views/modalDelete.blade.php
@@ -0,0 +1,22 @@
+<!-- ModalDelete -->
+<div class="modal fade" id="listModalDelete" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form id="deleteContent">
+                <div class="modal-header">
+                    <h5 class="modal-title">Удаление элемента</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    text
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                    <button type="submit" class="btn btn-primary" id="buttonForDelete">Подтвердить</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
